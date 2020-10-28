f505463762c511c335f8ba31cd0f745f299bdd8f
mzacki <zacki.mateusz@gmail.com>
2020-10-28 19:38:52 +0100

    order interface

---
+++ eshop/src/main/java/com/bee/shop/order/Order.java
@@ -0,0 +1,18 @@
+package com.bee.shop.order;
+
+import com.bee.shop.payment.PaymentMethod;
+import com.bee.shop.payment.PaymentVisitor;
+import com.bee.shop.product.Item;
+import com.bee.shop.shipping.ShippingOption;
+import java.math.BigDecimal;
+
+public interface Order {
+
+        void addItem(Item item);
+        void removeItem(Item item);
+        void setDiscountOnItem(Item item, BigDecimal discount);
+        void setShippingOption(ShippingOption shippingOption);
+        void setPaymentMethod(PaymentMethod paymentMethod);
+        void makePayment(PaymentVisitor paymentVisitor);
+
+}
