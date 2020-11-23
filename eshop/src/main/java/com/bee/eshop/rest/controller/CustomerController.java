8a266d43064feed772e2d2918d5f97df98ecaaf1
mzacki <zacki.mateusz@gmail.com>
2020-11-23 20:27:28 +0100

    CustomerController

--- eshop/src/main/java/com/bee/eshop/rest/controller/CustomerController.java
+++ eshop/src/main/java/com/bee/eshop/rest/controller/CustomerController.java
@@ -2,7 +2,17 @@ package com.bee.eshop.rest.controller;
 
 
 import com.bee.eshop.persistency.entities.Customer;
+import com.bee.eshop.persistency.filters.CustomerFilter;
 import com.bee.eshop.persistency.repo.CustomerRepository;
+import com.bee.eshop.service.CustomerService;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Matt on 10.09.2018 at 20:49.
@@ -12,25 +22,34 @@ import com.bee.eshop.persistency.repo.CustomerRepository;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
+    private final CustomerService customerService;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository,
+            CustomerService customerService) {
         this.customerRepository = customerRepository;
+        this.customerService = customerService;
     }
 
     @PostMapping
     public Customer create(@RequestBody Customer customer) {
-        return CustomerRepository.save(customer);
+        return customerRepository.save(customer);
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public Iterable<Customer> getAll() {
-        return CustomerRepository.findAll();
+        return customerRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Customer getById(@PathVariable int id) {
-        return CustomerRepository.findById(id).orElse(null);
+    public Customer getById(@PathVariable long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    //TODO return DTO form STRUCTURE instead of Entity directly
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Customer> getCustomers(CustomerFilter filter) {
+        return customerService.getCustomers(filter);
     }
 
 }
