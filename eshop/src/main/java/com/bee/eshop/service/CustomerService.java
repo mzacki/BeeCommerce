d131f30737344bb7e36a2b9322ced5fdb6eb62b2
mzacki <zacki.mateusz@gmail.com>
2020-11-23 20:29:07 +0100

    CustomerService

---
+++ eshop/src/main/java/com/bee/eshop/service/CustomerService.java
@@ -0,0 +1,23 @@
+package com.bee.eshop.service;
+
+import com.bee.eshop.persistency.entities.Customer;
+import com.bee.eshop.persistency.filters.CustomerFilter;
+import com.bee.eshop.persistency.repo.CustomerRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
+    public List<Customer> getCustomers(CustomerFilter filter) {
+        return customerRepository.findAll(filter.toSpecification());
+    }
+
+}
