7638bf7ee8caaad3243ce3612276f16432e18ff2
mzacki <zacki.mateusz@gmail.com>
2020-11-25 19:31:41 +0100

    Customer DTO

---
+++ eshop/src/main/java/com/bee/eshop/structure/customer/BusinessCustomer.java
@@ -0,0 +1,12 @@
+package com.bee.eshop.structure.customer;
+
+/**
+ * Created by Matt on 28.12.2018 at 17:36.
+ */
+
+public class BusinessCustomer extends Customer {
+
+    private double creditLimit;
+
+
+}
--- eshop/src/main/java/com/bee/eshop/structure/customer/Customer.java
+++ eshop/src/main/java/com/bee/eshop/structure/customer/Customer.java
@@ -1,5 +1,9 @@
 package com.bee.eshop.structure.customer;
 
+/**
+ * Created by Matt on 28.12.2018 at 16:56.
+ */
+
 public class Customer {
 
 }
